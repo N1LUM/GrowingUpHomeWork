866e92c504e040db69f469fe896d1bcd1c0063f8
Sergey Lazurenko <s.lazurenko@trucker.group>
2025-05-22 14:47:44 +0300

    Added hash convert realization

--- lib/commands/hash_convert.rb
+++ lib/commands/hash_convert.rb
@@ -5,5 +5,44 @@ class HashConvert
     @hash = hash
   end
 
-  def self.transform(input_hash); end
+  def self.transform_functional(obj)
+    case obj
+    when Hash
+      obj.each_with_object({}) do |(k, v), acc|
+        acc[k] = transform_functional(v)
+      end
+    when Array
+      obj.map { |el| transform_functional(el) }
+         .reduce({}) { |acc, h| acc.merge(h) }
+    else
+      unless obj.is_a?(Symbol) || obj.is_a?(String)
+        raise ArgumentError, "Unsupported value type: #{obj.class}"
+      end
+      { obj => {} }
+    end
+  end
+
+  def self.transform_imperative(obj)
+    if obj.is_a?(Hash)
+      result = {}
+      obj.each do |key, value|
+        result[key] = transform_imperative(value)
+      end
+      result
+    elsif obj.is_a?(Array)
+      result = {}
+      obj.each do |item|
+        transformed = transform_imperative(item)
+        transformed.each do |k, v|
+          result[k] = v
+        end
+      end
+      result
+    else
+      unless obj.is_a?(Symbol) || obj.is_a?(String)
+        raise ArgumentError, "Unsupported value type: #{obj.class}"
+      end
+      { obj => {} }
+    end
+  end
 end
--- spec/hash_convert_spec.rb
+++ spec/hash_convert_spec.rb
@@ -25,31 +25,41 @@ RSpec.describe HashConvert do
       }
     end
 
-    context "with valid input" do
-      it 'returned correct converted hash' do
-        expect(HashConvert.transform(input_hash)).to eq(expected_output)
+    let(:invalid_input) do
+      {
+        a: 333,
+        b: [:b1, :b2],
+        c: { c1: 444 },
+        d: [:d1, [:d2, :d3], { d4: "Text" }],
+        e: { e1: { e2: :e3 }, e4: [567, :e6] }
+      }
+    end
+
+    context 'functional style (.transform_functional)' do
+      it 'returns correct converted hash for valid input' do
+        expect(HashConvert.transform_functional(input_hash)).to eq(expected_output)
+      end
+
+      it 'raises error for invalid input' do
+        expect { HashConvert.transform_functional(invalid_input) }.to raise_error(ArgumentError)
+      end
+
+      it 'returns empty hash for empty input' do
+        expect(HashConvert.transform_functional({})).to eq({})
       end
     end
 
-    context 'with invalid input' do
-      let(:input_hash) do
-        {
-          a: 333,
-          b: [:b1, :b2],
-          c: { c1: 444 },
-          d: [:d1, [:d2, :d3], { d4: "Text" }],
-          e: { e1: { e2: :e3 }, e4: [567, :e6] }
-        }
+    context 'imperative style (.transform_imperative)' do
+      it 'returns correct converted hash for valid input' do
+        expect(HashConvert.transform_imperative(input_hash)).to eq(expected_output)
       end
 
-      it 'raises error for wrong types' do
-        expect { HashConvert.transform(input_hash) }.to raise_error(ArgumentError)
+      it 'raises error for invalid input' do
+        expect { HashConvert.transform_imperative(invalid_input) }.to raise_error(ArgumentError)
       end
-    end
 
-    context 'with empty input' do
-      it 'returns empty hash' do
-        expect(HashConvert.transform({})).to eq({})
+      it 'returns empty hash for empty input' do
+        expect(HashConvert.transform_imperative({})).to eq({})
       end
     end
   end
